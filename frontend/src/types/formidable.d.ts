e306c787ac69cf05ea3167702e1332b7c0f09d1e
Dami Vera-Cruz <94263586+dammyvc@users.noreply.github.com>
2025-04-03 00:41:56 +0100

    corrections

--- frontend/src/types/formidable.d.ts
+++ frontend/src/types/formidable.d.ts
@@ -1,22 +1,30 @@
+import { IncomingMessage } from "http";
+
 declare module "formidable" {
-    export default any;
+    
+    export interface Formidable {
+        parse: (
+            req: IncomingMessage, 
+            callback: (err: Error | null, fields: Fields, files: Files) => void
+        ) => void;
+    }
+
     export interface Fields {
         [key: string]: string[] | undefined;
     }
+
     export interface Files {
         [key: string]: File | File[] | undefined;
     }
+
     export interface File {
         filepath: string;
         originalFilename: string | null;
         mimetype: string | null;
         size: number;
     }
-    export interface Formidable {
-        parse: (
-            req: any,
-            callback: (err: any, fields: Fields, files: Files) => void
-        ) => void;
-    }
+
+    
     export function formidable(options?: { multiples?: boolean }): Formidable;
+    export default function formidable(options?: { multiples?: boolean }): Formidable;
 }
