1294867313d4ab6ab8fe0dd0f7609687ca197d8a
Dami Vera-Cruz <94263586+dammyvc@users.noreply.github.com>
2025-04-02 21:37:38 +0100

    Quiz page adjustment

--- frontend/src/app/(dashboard)/quizzes/phishing_quiz/attempt/page.tsx
+++ frontend/src/app/(dashboard)/quizzes/phishing_quiz/attempt/page.tsx
@@ -6,7 +6,7 @@ export default function PhishingAttempt() {
     <>
       <div className="pl-4 flex gap-4 flex-col md:flex-row mr-3 lg:mr-0 md:mr-0">
         {/* LEFT */}
-        <div className="w-full lg:w-2/3">
+        <div className="w-full">
           <Suspense fallback={<div>Loading quiz...</div>}>
             <QuizAttempt />
           </Suspense>
